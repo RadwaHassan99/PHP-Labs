46e3d01c6d74733a44890b15c669da9ccf7d409e
Radwa Hassan <63107268+RadwaHassan99@users.noreply.github.com>
2023-03-08 23:30:16 +0200

    lab1

---
+++ Lab1/README.md
@@ -0,0 +1,21 @@
+## 1-Valid data:
+![image](https://user-images.githubusercontent.com/63107268/223413887-4aa39376-279f-4024-a79a-7db458c60aa3.png)
+![image](https://user-images.githubusercontent.com/63107268/223413948-6aebe88c-20a1-4994-b4a5-cf3cbc41820a.png)
+
+
+## 2-empty field:
+![image](https://user-images.githubusercontent.com/63107268/223414140-9a895922-dbdf-4d90-959b-6a4819b529eb.png)
+![image](https://user-images.githubusercontent.com/63107268/223414994-bad6a6a6-a51b-43b8-b759-43c5453a38e0.png)
+![image](https://user-images.githubusercontent.com/63107268/223415048-e97fa74a-0229-4716-8c5b-32e31d70a07a.png)
+
+
+## 3-Invalid mail:
+![image](https://user-images.githubusercontent.com/63107268/223414262-a7bece93-ac07-4cfe-b39a-51e7ce1b41f8.png)
+
+
+## 4-name exceeds 100chs:
+![image](https://user-images.githubusercontent.com/63107268/223414438-e330cf41-0dad-4e03-9280-bd9e86d711bc.png)
+
+
+## 5-msg exceeds 255 chs:
+![image](https://user-images.githubusercontent.com/63107268/223414817-7748c701-c6b7-4559-a5ac-8fd3414a3543.png)
---
+++ Lab1/views/form.php
@@ -0,0 +1,66 @@
+<html>
+    <head>
+        <title> contact form </title>
+        <style>
+input[type=text] {
+  width: 100%;
+  padding: 12px 20px;
+  margin: 10px auto;
+  display: inline-block;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  box-sizing: border-box;
+}
+
+input[type=submit] {
+  width:10%;
+  background-color: #4CAF50;
+  color: white;
+  padding: 14px 20px;
+  margin: 8px 0;
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
+}
+
+input[type=submit]:hover {
+  background-color: #45a049;
+}
+
+#contact_form {
+  border-radius: 5px;
+  background-color: #f2f2f2;
+  padding: 20px;
+}
+</style>
+    </head>
+
+    <body>
+        <h3> Contact Form </h3>
+        <div id="after_submit">
+            <?php echo $error ?? NULL ?>
+        </div>
+        <form id="contact_form" action="index.php" method="POST" enctype="multipart/form-data">
+                         
+            <div class="row">
+                <label class="required" for="name">Your name:</label><br />
+                <input id="name" class="input" name="name" type="text" value="<?php echo  Validate_Form("name") ?>" size="30" /><br />
+
+            </div>
+            <div class="row">
+                <label class="required" for="email">Your email:</label><br />
+                <input id="email" class="input" name="email" type="text" value="<?php echo  Validate_Form("email") ?>" size="30" /><br />
+            </div>
+            <div class="row">
+                <label class="required" for="message">Your message:</label><br />
+                <textarea id="message" class="input" name="message" rows="7" cols="30"><?php echo  Validate_Form("message") ?></textarea><br />
+
+            </div>
+
+            <input id="submit" name="submit" type="submit" value="Send" />
+
+
+        </form>
+    </body>
+
+</html>
